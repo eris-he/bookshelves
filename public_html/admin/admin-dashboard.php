a5a920821d23d20b00066eeed13929fbc0b1d69a
eris-he <foreverphysics7@gmail.com>
2023-09-07 14:00:33 -0500

    small changes

--- public_html/admin/admin-dashboard.php
+++ public_html/admin/admin-dashboard.php
@@ -1,11 +1,20 @@
 <?php
     require '../imports.php';
+
+    //only display page if user is logged in and is admin
+
+    if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false) {
+        header("Location: /errors/unprivileged-user.php");
+    }
 ?>
 
 <!DOCTYPE html>
 <!-- Create cards that link to different admin pages -->
 <div class="footer-wrap">
     <div class="body-template">
+        <div>
+            <h1>Admin Dashboard</h1>
+        </div>
     </div>
     <?php
         require '../footer.php';
---
+++ public_html/errors/unprivileged-user.php
@@ -0,0 +1,12 @@
+<?php
+require_once "../imports.php";
+?>
+
+<!DOCTYPE html>
+<div class="footer-wrap">
+    <div class="body-template">
+        <div class="center">
+            <h1>Warning!</h1>
+            <h2>You are not authorized to view that page!</h2>
+        </div>
+</div>
